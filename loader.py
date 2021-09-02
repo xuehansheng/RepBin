a5feda0ef24245dba14134571c60943235325ad7
Hansheng Xue <814216169@qq.com>
2021-09-02 11:52:10 +0800

    Updata bidirectionalmap

--- loader.py
+++ loader.py
@@ -7,10 +7,41 @@ import networkx as nx
 import scipy.sparse as sp
 from collections import Counter
 from collections import defaultdict
-from bidirectionalmap import BidirectionalMap
 from Bio import SeqIO
 from igraph import *
 
+
+class BidirectionalError(Exception):
+	"""Must set a unique value in a BijectiveMap."""
+	def __init__(self, value):
+		self.value = value
+		msg = 'The value "{}" is already in the mapping.'
+		super().__init__(msg.format(value))
+
+class BidirectionalMap(dict):
+	"""Invertible map."""
+
+	def __init__(self, inverse=None):
+		if inverse is None:
+			inverse = self.__class__(inverse=self)
+		self.inverse = inverse
+
+	def __setitem__(self, key, value):
+		if value in self.inverse:
+			raise BidirectionalError(value)
+
+		self.inverse._set_item(value, key)
+		self._set_item(key, value)
+
+	def __delitem__(self, key):
+		self.inverse._del_item(self[key])
+		self._del_item(key)
+
+	def _del_item(self, key):
+		super().__delitem__(key)
+
+	def _set_item(self, key, value):
+		super().__setitem__(key, value)
 
 def remove_duplicates(constraints):
 	for line in constraints:
